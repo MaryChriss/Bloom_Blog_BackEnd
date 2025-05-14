3250e0d3d0d22edb80b10d694483527eea8f8d3f
Mariana Christina <161539215+MaryChriss@users.noreply.github.com>
2025-05-14 20:41:18 -0300

    ajuste de login

--- src/main/java/br/com/fiap/Bloom/controller/UserController.java
+++ src/main/java/br/com/fiap/Bloom/controller/UserController.java
@@ -2,11 +2,14 @@ package br.com.fiap.Bloom.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fiap.Bloom.dto.UserUpdateDTO;
 import br.com.fiap.Bloom.model.User;
 import br.com.fiap.Bloom.repository.UserRepository;
 import jakarta.validation.Valid;
@@ -26,5 +29,21 @@ public class UserController {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return repository.save(user);
     }
+
+    @PutMapping("/{id}")
+public User update(@PathVariable Long id, @RequestBody @Valid UserUpdateDTO dto) {
+    User user = repository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+    if (dto.getEmail() != null && !dto.getEmail().isBlank()) {
+        user.setEmail(dto.getEmail());
+    }
+
+    if (dto.getPassword() != null && !dto.getPassword().isBlank()) {
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+    }
+
+    return repository.save(user);
+}
 
 }
